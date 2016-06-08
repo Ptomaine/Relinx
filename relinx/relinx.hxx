7d2d5ce371e3bc7a1ce4b0a6635c1ad7c09f80f2
Arlen Keshabyan <arlen.albert@gmail.com>
2016-06-09 02:59:58 +0300

    Moving pragma once compiler directive to the first line

--- relinx/relinx.hxx
+++ relinx/relinx.hxx
@@ -1,3 +1,5 @@
+#pragma once
+
 /*
 MIT License
 
@@ -21,8 +23,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
-#pragma once
 
 #include <algorithm>
 #include <iostream>
